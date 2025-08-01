f5f3759040afc3e6116f3c1612d67efc18833c0b
Carlos Javier <carlosjavi1887@gmail.com>
2025-08-01 07:49:21 -0500

    Cree todos los model de la db V1

---
+++ backend/futBattle/src/main/java/com/example/futBattle/models/Player.java
@@ -0,0 +1,26 @@
+package com.example.futBattle.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity(name = "player")
+public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true, length = 50)
+    private String name;
+
+}
